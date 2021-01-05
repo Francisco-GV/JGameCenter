d6210c0c1cf625ad76e416f050d2120806c7f6f7
Frank <francisco_gasca@hotmail.com>
2021-01-05 16:43:35 -0600

    Test game, will be a full game soon

---
+++ src/com/frank/jgamecenter/games/Dodger.java
@@ -0,0 +1,20 @@
+package com.frank.jgamecenter.games;
+
+public class Dodger extends Game {
+    public Dodger() {
+        super("Dodger", """
+                Several bad guys fall from the top of the screen,
+                 and the user must avoid them.
+                """, null);
+    }
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
