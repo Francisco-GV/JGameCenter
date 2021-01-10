db79807911e66ac11825f10d46b6847e4dd8c25a
Frank <francisco_gasca@hotmail.com>
2021-01-09 19:16:46 -0600

    Added some drawing methods

---
+++ src/com/frank/jgamecenter/games/DodgerGame.java
@@ -0,0 +1,178 @@
+package com.frank.jgamecenter.games;
+
+import com.frank.jgamecenter.games.resources.Element;
+import com.frank.jgamecenter.games.resources.Game;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class DodgerGame extends Game {
+    private double width;
+    private double height;
+
+    private List<Obstacle> obstacleList;
+    private Spacecraft spacecraft;
+
+
+    private boolean toLeft = false;
+    private boolean toRight = false;
+
+    private boolean stop = false;
+
+    private int totalLives;
+    private int lives;
+
+    private Image imgBackground;
+    private Image imgSpacecraft;
+    private Image imgHeart;
+    private Image imgHeartEmpty;
+    private Image imgObstacle;
+
+    @Override
+    public void init() {
+        totalLives = 5;
+        lives = totalLives;
+        getCanvas().addEventHandler(KeyEvent.KEY_PRESSED, evt -> {
+            KeyCode code = evt.getCode();
+            if (code == KeyCode.LEFT || code == KeyCode.A) {
+                if (spacecraft.x - spacecraft.verticalSpeed > 0) {
+                    toLeft = true;
+                    toRight = false;
+                }
+            } else if (code == KeyCode.RIGHT || code == KeyCode.D) {
+                if (spacecraft.x + spacecraft.width + spacecraft.verticalSpeed < width) {
+                    toRight = true;
+                    toLeft = false;
+                }
+            }
+        });
+
+        getCanvas().addEventHandler(KeyEvent.KEY_RELEASED, evt -> {
+            switch (evt.getCode()) {
+                case LEFT, A    -> toLeft = false;
+                case RIGHT, D   -> toRight = false;
+            }
+        });
+
+        width = g.getCanvas().getWidth();
+        height = g.getCanvas().getHeight();
+
+        obstacleList = new CopyOnWriteArrayList<>();
+
+        spacecraft = new Spacecraft();
+
+        imgBackground   = loadImage("/resources/images/dodger/space-galaxy-background.jpg");
+        imgHeart        = loadImage("/resources/images/dodger/heart.png", 32);
+        imgHeartEmpty   = loadImage("/resources/images/dodger/heart_empty.png", 32);
+        imgSpacecraft   = loadImage("/resources/images/dodger/spacecraft.png", 64);
+        imgObstacle     = loadImage("/resources/images/dodger/asteroid.png", 64);
+    }
+
+    private void generateObstacle() {
+        obstacleList.add(new Obstacle());
+    }
+
+    private void evaluatePosition(Obstacle obstacle) {
+        if (obstacle.y > height) {
+            obstacleList.remove(obstacle);
+        }
+    }
+
+    @Override
+    public void run() {
+        if (!stop) {
+            if (toLeft) {
+                spacecraft.x -= spacecraft.verticalSpeed;
+            }
+            if (toRight) {
+                spacecraft.x += spacecraft.verticalSpeed;
+            }
+            evaluateCollision();
+        }
+
+        drawBackground();
+
+        for (Obstacle obstacle : obstacleList) {
+            drawObstacle(obstacle);
+        }
+
+        g.drawImage(this.imgSpacecraft, spacecraft.x, spacecraft.y, spacecraft.width, spacecraft.height);
+        drawHearts();
+
+        int probability = (int) (Math.random() * 100);
+        if (probability >= 50 && probability <= 53) {
+            generateObstacle();
+        }
+    }
+
+    private void drawBackground() {
+        g.drawImage(imgBackground, 0, 0, width, height);
+    }
+
+    private void drawObstacle(Obstacle obstacle) {
+        g.drawImage(imgObstacle, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+    }
+
+    private void drawHearts() {
+        int heartX = 5;
+        int heartY = 5;
+        int size = 32;
+        for (int i = 0; i < lives; i++) {
+            g.drawImage(imgHeart, heartX, heartY, size, size);
+            heartX += size + 5;
+        }
+        for (int i = 0; i < totalLives - lives; i++) {
+            g.drawImage(imgHeartEmpty, heartX, heartY, size, size);
+            heartX += size + 5;
+        }
+    }
+
+    private void evaluateCollision() {
+        for (Obstacle obstacle : obstacleList) {
+            obstacle.y += obstacle.speed;
+            evaluatePosition(obstacle);
+            if (spacecraft.collides(obstacle) && !obstacle.alreadyCollided) {
+                obstacle.alreadyCollided = true;
+                lives--;
+                if (lives == 0) {
+                    stop = true;
+                }
+            }
+        }
+    }
+
+    private class Obstacle extends Element {
+        public double speed;
+        public boolean alreadyCollided;
+        public Obstacle() {
+            double size = Math.random() * 25 + 25;
+            this.x = Math.random() * DodgerGame.this.width;
+            this.y = (Math.random() * -100) - size;
+            this.width = size;
+            this.height = size;
+            this.speed = (Math.random() * 2) + 1;
+        }
+    }
+
+    private class Spacecraft extends Element {
+        public double verticalSpeed;
+        public Spacecraft() {
+            this.width = 64;
+            this.height = 64;
+            this.x = DodgerGame.this.width / 2 - this.width / 2;
+            this.y = DodgerGame.this.height - this.height - 25;
+
+            this.verticalSpeed = 6.5;
+        }
+    }
+
+    public DodgerGame() {
+        super("Dodger", """
+                Several asteroids fall from the 
+                top of the screen, and the user 
+                must avoid them.""", loadThumbnail("dodger.png"));
+    }
+}
