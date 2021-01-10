9498763c72f1b343d74560131ab21780551a043d
Frank <francisco_gasca@hotmail.com>
2021-01-09 19:16:01 -0600

    Deleted image field

---
+++ src/com/frank/jgamecenter/games/resources/Element.java
@@ -0,0 +1,15 @@
+package com.frank.jgamecenter.games.resources;
+
+public class Element {
+    public double x;
+    public double y;
+    public double width;
+    public double height;
+
+    public boolean collides(Element element) {
+        return  ((this.x >= element.x && this.x <= element.x + element.width) ||
+                  element.x >= this.x && element.x <= this.x + this.width) &&
+                ((this.y >= element.y && this.y <= element.y + element.height) ||
+                        element.y >= this.y && element.y <= this.y + this.height);
+    }
+}
