3e9e5d6079511e95d8e0e52c8e016e09a64425c6
Frank <francisco_gasca@hotmail.com>
2021-01-11 01:29:50 -0600

    Added a new pause menu that can be open in every game

--- src/com/frank/jgamecenter/MainGUI.java
+++ src/com/frank/jgamecenter/MainGUI.java
@@ -3,14 +3,19 @@ package com.frank.jgamecenter;
 import com.frank.jgamecenter.games.resources.Game;
 
 import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Group;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.canvas.Canvas;
+import javafx.scene.SnapshotParameters;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
@@ -21,30 +26,41 @@ import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+import javax.imageio.ImageIO;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
 
 public class MainGUI {
-    @FXML private AnchorPane menuPane;
-    @FXML private FlowPane gamesContainer;
+    @FXML private AnchorPane mainMenuPane;
+    @FXML private FlowPane gamesListContainer;
 
+    private StackPane gameContainer;
     private Stage primaryStage;
+    private Scene mainMenuScene;
+    private Node inGameMenu;
 
     @FXML private void initialize() throws IOException {
         for (Game game : JGameCenter.getInstance().getGameList()) {
-            gamesContainer.getChildren().add(createGameElement(game));
+            gamesListContainer.getChildren().add(createGameElement(game));
         }
     }
 
     public void setPrimaryStage(Stage primaryStage) {
         this.primaryStage = primaryStage;
         primaryStage.sceneProperty().addListener(
-                (obs, old, newScene) -> primaryStage.setResizable(newScene.getRoot() == menuPane)
+                (obs, old, newScene) -> primaryStage.setResizable(newScene.getRoot() == mainMenuPane)
         );
+    }
+
+    public void setPrimaryScene(Scene scene) {
+        this.mainMenuScene = scene;
     }
 
     private Region createGameElement(Game game) throws IOException {
@@ -53,19 +69,68 @@ public class MainGUI {
         return loader.load();
     }
 
-    private void changeToCanvas(Canvas canvas) {
-        canvas.setFocusTraversable(true);
-        canvas.requestFocus();
+    private void initInGameMenu(Game game) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/resources/fxml/in_game_menu.fxml"));
+        loader.setController(new InGameMenu(game));
+        inGameMenu = loader.load();
+    }
+
+    private void changeToGame(Game game) throws IOException {
+        gameContainer = new StackPane();
+
+        gameContainer.setAlignment(Pos.CENTER);
+
+        Node gameNode = game.getGameNode();
+        gameNode.setFocusTraversable(true);
+        gameNode.requestFocus();
+
+        initInGameMenu(game);
+        inGameMenu.setDisable(true);
+        inGameMenu.setVisible(false);
+
+        gameContainer.getChildren().add(0, gameNode);
+        gameContainer.getChildren().add(1, inGameMenu);
+
+        gameNode.setOnKeyReleased(evt -> {
+            if (evt.getCode() == KeyCode.ESCAPE || evt.getCode() == KeyCode.P) {
+                game.setPause(true);
+                Platform.runLater(() -> {
+                    inGameMenu.setDisable(false);
+                    inGameMenu.setVisible(true);
+                    inGameMenu.requestFocus();
+                });
+            } else if (evt.getCode() == KeyCode.F5) {
+                takeSnapshot(gameNode);
+            }
+        });
+
         Platform.runLater(() -> {
-            primaryStage.setScene(new Scene(new Group(canvas)));
+            primaryStage.setScene(new Scene(gameContainer));
             primaryStage.centerOnScreen();
         });
     }
 
     private void changeToMenu() {
-        Platform.runLater(() -> primaryStage.setScene(new Scene(menuPane)));
+        Platform.runLater(() -> {
+            primaryStage.setScene(mainMenuScene);
+            primaryStage.centerOnScreen();
+            gameContainer = null;
+        });
     }
 
+    private void takeSnapshot(Node gameNode) {
+        WritableImage image = gameNode.snapshot(new SnapshotParameters(), null);
+
+        File directory = new File(System.getProperty("user.home"));
+        File output = new File(directory.getAbsolutePath() + "/desktop/snapshot" + new Date().getTime() + ".png");
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", output);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SuppressWarnings("unused")
     public class GameElement {
         @FXML private Label lblTitle;
         @FXML private Pane root;
@@ -78,7 +143,13 @@ public class MainGUI {
         }
 
         @FXML private void initialize() {
-            root.setOnMouseReleased(evt -> start());
+            root.setOnMouseReleased(evt -> {
+                try {
+                    start();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
 
             lblTitle.setText(game.getName());
             Tooltip tooltip = new Tooltip(game.getDescription());
@@ -93,8 +164,8 @@ public class MainGUI {
             }
         }
 
-        @FXML private void start() {
-            changeToCanvas(game.getCanvas());
+        @FXML private void start() throws IOException {
+            changeToGame(game);
             primaryStage.setTitle(JGameCenter.APP_TITLE + " - " + game.getName());
             game.start();
         }
@@ -135,6 +206,38 @@ public class MainGUI {
             InputStream inputStream = MainGUI.GameElement.class.getResourceAsStream("/resources/images/game-controller-128x128.png");
             Image image = new Image(inputStream, 64, 64, true, true);
             createThumbnail(root, image);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public class InGameMenu {
+        @FXML private Button btnResume;
+        @FXML private Button btnRestart;
+        @FXML private Button btnExit;
+
+        private final Game game;
+        public InGameMenu(Game game) {
+            this.game = game;
+        }
+
+        @FXML private void initialize() {
+            btnResume.setOnAction(e -> resume());
+            btnRestart.setOnAction(e -> game.restart());
+            btnExit.setOnAction(e -> backToMenu());
+        }
+
+        private void resume() {
+            game.setPause(false);
+            inGameMenu.setDisable(true);
+            inGameMenu.setVisible(false);
+
+            Node gameNode = gameContainer.getChildren().get(0);
+            gameNode.requestFocus();
+        }
+
+        private void backToMenu() {
+            game.stop();
+            changeToMenu();
         }
     }
 }
