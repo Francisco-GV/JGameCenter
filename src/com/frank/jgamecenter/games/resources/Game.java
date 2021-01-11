ac682a5480b8bd64af50ae5c9e547ce0ae72674f
Frank <francisco_gasca@hotmail.com>
2021-01-11 01:22:36 -0600

    Canvas and graphics elements moved to new GraphicGame subclass

--- src/com/frank/jgamecenter/games/resources/Game.java
+++ src/com/frank/jgamecenter/games/resources/Game.java
@@ -1,55 +1,29 @@
 package com.frank.jgamecenter.games.resources;
 
 import com.frank.jgamecenter.games.SnakeGame;
-import javafx.animation.AnimationTimer;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.SnapshotParameters;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
-import javafx.scene.image.WritableImage;
-import javafx.scene.input.KeyCode;
-
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStream;
-import java.util.Date;
 
 public abstract class Game {
     private final String name;
     private final String description;
     private final Image thumbnail;
 
-    private final Canvas canvas;
-    protected final GraphicsContext g;
+    protected Node gameNode;
+
+    protected boolean isPaused;
+
     public Game(String name, String description, Image thumbnail) {
         this.name = name;
         this.description = description;
         this.thumbnail = thumbnail;
-
-        canvas = new Canvas(775, 450);
-        g = canvas.getGraphicsContext2D();
-
-        getCanvas().setOnKeyReleased(evt -> {
-            if (evt.getCode() == KeyCode.R) {
-                init();
-            } else if (evt.getCode() == KeyCode.F5) {
-                WritableImage image = getCanvas().snapshot(new SnapshotParameters(), null);
-
-                File output = new File("snapshot" + new Date().getTime() + ".png");
-                try {
-                    ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", output);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+        this.isPaused = false;
     }
 
     public static Image loadImage(String path) {
         InputStream inputStream = SnakeGame.class.getResourceAsStream(path);
-        return new Image(inputStream);
+        return new Image(inputStream, 0, 0, true, true);
     }
 
     public static Image loadImage(String path, int size) {
@@ -69,27 +43,8 @@ public abstract class Game {
         return thumbnail;
     }
 
-    public Canvas getCanvas() {
-        return canvas;
-    }
-
-    public void start() {
-        canvas.requestFocus();
-        canvas.setFocusTraversable(true);
-        init();
-        AnimationTimer animationTimer = new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-                refresh();
-                run();
-            }
-        };
-
-        animationTimer.start();
-    }
-
-    protected void refresh() {
-        g.clearRect(0, 0, g.getCanvas().getWidth(), g.getCanvas().getHeight());
+    public Node getGameNode() {
+        return gameNode;
     }
 
     protected static Image loadThumbnail(String name) {
@@ -97,5 +52,11 @@ public abstract class Game {
     }
 
     protected abstract void init();
-    protected abstract void run();
+    public abstract void start();
+    public abstract void restart();
+    public abstract void stop();
+
+    public void setPause(boolean pause) {
+        this.isPaused = pause;
+    }
 }
