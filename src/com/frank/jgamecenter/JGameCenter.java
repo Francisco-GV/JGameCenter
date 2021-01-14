1222de31391ac14eeb63c609cb379031e7ab00c2
Frank <francisco_gasca@hotmail.com>
2021-01-14 16:19:38 -0600

    Games list change to GameInitializer type and other minor changes

--- src/com/frank/jgamecenter/JGameCenter.java
+++ src/com/frank/jgamecenter/JGameCenter.java
@@ -1,7 +1,8 @@
 package com.frank.jgamecenter;
 
-import com.frank.jgamecenter.games.*;
-import com.frank.jgamecenter.games.resources.Game;
+import com.frank.jgamecenter.game.GameInitializer;
+import com.frank.jgamecenter.game.games.*;
+import com.frank.jgamecenter.game.Game;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,21 +13,53 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class JGameCenter extends Application {
     public static final String APP_TITLE = "JGameCenter";
-    private List<Game> gameList;
+    private List<GameInitializer> gameList;
 
     @Override
     public void init() {
         Font.loadFont(getClass().getResourceAsStream("/resources/font/arcade_ya/ARCADE_N.TTF"), 30);
-
         instance = this;
         gameList = new ArrayList<>();
-        gameList.add(new DodgerGame());
-        //gameList.add(new MemoryGame()); next game
-        gameList.add(new SnakeGame());
+        addNewGame(SnakeGame.class, createData("Snake", """
+            The player controls a snake that
+            moves around the board. The snake
+            must try to eat apples that 
+            randomly appear.""",
+            GameInitializer.loadThumbnail("snake.png")));
+
+        addNewGame(DodgerGame.class, createData("Dodger", """
+            Several asteroids fall from the 
+            top of the screen, and the user 
+            must avoid them.""",
+            GameInitializer.loadThumbnail("dodger.png")));
+
+        addNewGame(MemoryGame.class, createData("Memory","""
+            A board full of overturned cards.
+            There is a pair for each card.
+            The player needs to overturn all
+            the cards.""",
+            null));
+
+    }
+
+    private Map<String, Object> createData(String title, String description, Image thumbnail) {
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("title", title);
+        map.put("description", description);
+        map.put("thumbnail", thumbnail);
+
+        return map;
+    }
+
+    public void addNewGame(Class<? extends Game> gameClass, Map<String, Object> gameData) {
+        gameList.add(new GameInitializer(gameClass, gameData));
     }
 
     @Override
@@ -35,6 +68,8 @@ public class JGameCenter extends Application {
         Parent parent = loader.load();
         MainGUI controller = loader.getController();
         controller.setPrimaryStage(primaryStage);
+
+        controller.initializeEffects();
 
         Scene scene = new Scene(parent);
 
@@ -53,7 +88,7 @@ public class JGameCenter extends Application {
         Application.launch(args);
     }
 
-    public List<Game> getGameList() {
+    public List<GameInitializer> getGameList() {
         return gameList;
     }
 
