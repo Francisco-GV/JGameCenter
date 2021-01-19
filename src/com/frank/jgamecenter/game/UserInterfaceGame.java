e28f74890414a25eef550c30d4228f08bf48fa91
Frank <francisco_gasca@hotmail.com>
2021-01-19 16:27:03 -0600

    Child of game that represents all those games with an GUI interface

---
+++ src/com/frank/jgamecenter/game/UserInterfaceGame.java
@@ -0,0 +1,10 @@
+package com.frank.jgamecenter.game;
+
+public abstract class UserInterfaceGame extends Game {
+    public UserInterfaceGame(String name) {
+        super(name);
+    }
+
+    @Override
+    public void stop() { }
+}
