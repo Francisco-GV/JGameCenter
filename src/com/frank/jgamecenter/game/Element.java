1e348924976e17ed386533a62dc48bd5ae688623
Frank <francisco_gasca@hotmail.com>
2021-01-19 16:28:38 -0600

    Added a constructor with all attributes

--- src/com/frank/jgamecenter/game/Element.java
+++ src/com/frank/jgamecenter/game/Element.java
@@ -6,10 +6,23 @@ public class Element {
     public double width;
     public double height;
 
+    public Element() { }
+
+    public Element(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
     public boolean collides(Element element) {
         return  ((this.x >= element.x && this.x <= element.x + element.width) ||
                   element.x >= this.x && element.x <= this.x + this.width) &&
                 ((this.y >= element.y && this.y <= element.y + element.height) ||
                         element.y >= this.y && element.y <= this.y + this.height);
+    }
+
+    public String getAttributesString() {
+        return "x: " + x + ", y: " + y + ", width: " + width + ", height: " + height;
     }
 }
